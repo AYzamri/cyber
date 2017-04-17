3c31f08b8573471f53a7ecb4878f7bdf7a8d19c8
Adam Zamri <zamri@post.bgu.ac.il>
2017-04-17 14:17:42 +0300

    added funcs

--- src/ass2.java
+++ src/ass2.java
@@ -1,15 +1,55 @@
+import java.util.*;
+
 /**
  * Created by adamz on 17/04/2017.
  */
 public class ass2
 {
-    
-
-    public static void main (String[] args){
-    
+    private static Map<String,String> _flags = new HashMap<String,String>();
+    private static Set _flagsWithValues = new HashSet<String>(Arrays.asList("-a","-c","-t","-k","-v","-o"));
+    private static boolean runAlgorithem(String algo){
+    //run algorithem sub_cbc_10
+        if(algo=="sub_cbc_10"){
+            if(_flags.get("-c")=="encryption")
+                run_CBC10_EncryptionAction();
+            else if(_flags.get("-c")=="decryption")
+                run_CBC10_DecryptionAction();
+            else{
+                System.out.println("Please enter valid value for action in subs_cbc_10");
+            }
+        
+        
+        
+        
+        
+        }
+    }
+    private static void run_CBC10_EncryptionAction(){
     
     
     
     
     }
+    private static void run_CBC10_DecryptionAction(){
+    
+    
+    
+    }
+    
+    public static void main (String[] args){
+        for (int n = 0; n < args.length; n++)
+        {
+            if (args[n].charAt(0) == '-')
+            {
+                String name = args[n];
+                String value = null;
+                if (_flagsWithValues.contains(args[n]) && n < args.length - 1)
+                    value = args[++n];
+                _flags.put(name, value);
+            }
+        }
+        runAlgorithem(_flags.get("-a"));
+    }
+    
+
 }
